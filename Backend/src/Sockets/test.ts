adb745e3fa0803bebc2da253b353d5d328bbb8b1
Sultan2403 <sultanremilekun@gmail.com>
2026-06-13 05:23:14 +0100

    fixed server side issues

--- Backend/src/Sockets/test.ts
+++ Backend/src/Sockets/test.ts
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
-import { MESSAGE_EVENTS } from "../Config/constants";
 
 const socket = io("http://localhost:5000"); // On frontend this'll be process.env.VITE_API_URL
 
