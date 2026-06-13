8cf632b90f11f52c0d62c4d85a704df21cd5d412
Sultan2403 <sultanremilekun@gmail.com>
2026-06-13 01:29:56 +0100

    formatting fixes

--- Backend/src/Apis/api.ts
+++ Backend/src/Apis/api.ts
@@ -1,23 +1,23 @@
-import axios from "axios"
-import fs from "fs"
-import { env } from "../Config/env"
+import axios from "axios";
+import fs from "fs";
+import { env } from "../Config/env";
 
 const api = axios.create({
-    baseURL: "https://v3.football.api-sports.io",
-    headers: {
-        "x-apisports-key": env.API_FOOTBALL_SECRET_KEY
-    }
-})
+  baseURL: "https://v3.football.api-sports.io",
+  headers: {
+    "x-apisports-key": env.API_FOOTBALL_SECRET_KEY,
+  },
+});
 
 async function testApi() {
-const stuff = await api.get("/fixtures?league=39&season=2022")
-const deepClone = JSON.parse(JSON.stringify(stuff.data))
-console.log(deepClone)
+  const stuff = await api.get("/fixtures?league=39&season=2022");
+  const deepClone = JSON.parse(JSON.stringify(stuff.data));
+  console.log(deepClone);
 
-// write res to file
+  // write res to file
 
-fs.writeFileSync("fixtures.json", JSON.stringify(deepClone))
+  fs.writeFileSync("fixtures.json", JSON.stringify(deepClone));
 }
-testApi()
+testApi();
 
-export default api
+export default api;
