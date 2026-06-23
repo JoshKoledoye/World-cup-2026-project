0746c4dbc701e3878eb51528b39bd11485e553a7
Sultan2403 <sultanremilekun@gmail.com>
2026-06-23 23:37:14 +0100

    added comment stating only admin should be allowd to create a poll

--- Backend/src/Services/polls.service.ts
+++ Backend/src/Services/polls.service.ts
@@ -3,6 +3,10 @@ import pollsCollection from "../DB/Models/polls.model";
 
 export const addPoll = async (poll: Poll): Promise<{ success: boolean }> => {
   try {
+    // Only admins can add polls by the way. 
+
+    // We'll implement that tmr. 
+
     const newPoll = await pollsCollection.create(poll);
     return { success: true };
   } catch (error) {
