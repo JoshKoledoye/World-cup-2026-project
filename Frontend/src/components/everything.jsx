efce41053ae1e68f44b3fe60e001cad0df4d1448
Josh koledoye <169603297+JoshKoledoye@users.noreply.github.com>
2026-06-24 14:29:33 +0100

    Change title color to white
    Updated the text color of the title to white.

--- Frontend/src/components/everything.jsx
+++ Frontend/src/components/everything.jsx
@@ -1017,7 +1017,7 @@ void main() {
           />
         </div>
         <div className="flex flex-col gap-4 items-center md:items-start">
-          <h1 className="font-display-lg text-headline-lg text-on-primary-fixed uppercase">
+          <h1 className="font-display-lg text-white text-headline-lg text-on-primary-fixed uppercase">
             TriOnda 26
           </h1>
           <p className="font-body-md text-on-primary-fixed-variant text-center md:text-left max-w-sm">
